690178b220e132f45c695d36c758f2e9ca48bed6
Coen Munckhof <c.munckhof@gmail.com>
2018-06-04 23:20:00 +0200

    Changed Z85 to only implement netstandard1.1; Updated Appveyor.

--- src/Z85e.Test/Z85ExtendedTest.cs
+++ src/Z85e.Test/Z85ExtendedTest.cs
@@ -9,7 +9,6 @@ namespace CoenM.Encoding.Test
         private readonly byte[] _helloWorldBytes = { 0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B };
         private const string HelloWorldString = "HelloWorld";
 
-        
         [Fact]
         public void HelloWorldDecodeTest()
         {
