7546716905bf11eb4d42de05b68e5d16c5bd1a5b
Coen Munckhof <c.munckhof@gmail.com>
2018-09-08 23:19:35 +0200

    Fixed test decode z85 span

--- src/Z85e.Test/TestData/AllDecodeScenarios.cs
+++ src/Z85e.Test/TestData/AllDecodeScenarios.cs
@@ -13,8 +13,6 @@
 
             Add(Input(encodedString, false), doneResult);
             Add(Input(encodedString, true), doneResult);
-            Add(Input(encodedString, false), doneResult);
-            Add(Input(encodedString, true), doneResult);
 
 
             // Decode 1 character.
@@ -23,8 +21,6 @@
             var invalidDataResult = new DecodeExpectedData(OperationStatus.InvalidData, 0, 0, new byte[0]);
             encodedString = Z85eSampleData.GetHelloString(1);
 
-            Add(Input(encodedString, false), needMoreDataResult);
-            Add(Input(encodedString, true), invalidDataResult);
             Add(Input(encodedString, false), needMoreDataResult);
             Add(Input(encodedString, true), invalidDataResult);
 
@@ -36,8 +32,6 @@
 
             Add(Input(encodedString, false), needMoreDataResult);
             Add(Input(encodedString, true), paddedOneByteFinalResult); // different
-            Add(Input(encodedString, false), needMoreDataResult);
-            Add(Input(encodedString, true), invalidDataResult);
 
             //
             // Decode 3 chars.
@@ -47,8 +41,6 @@
 
             Add(Input(encodedString, false), needMoreDataResult);
             Add(Input(encodedString, true), paddedTwoByteFinalResult); // different
-            Add(Input(encodedString, false), needMoreDataResult);
-            Add(Input(encodedString, true), invalidDataResult);
 
             //
             // Decode 4 chars.
@@ -58,19 +50,13 @@
 
             Add(Input(encodedString, false), needMoreDataResult);
             Add(Input(encodedString, true), paddedThreeByteFinalResult); // different
-            Add(Input(encodedString, false), needMoreDataResult);
-            Add(Input(encodedString, true), invalidDataResult);
 
             //
             // Decode 5 chars.
-            // This is allowed in strict Z85
-            // In all cases the result should be the same.
             //
             encodedString = Z85eSampleData.GetHelloString(5);
             var helloDecodedDoneResult = new DecodeExpectedData(OperationStatus.Done, 5, 4, Z85eSampleData.HelloWorldBytes(4));
 
-            Add(Input(encodedString, false), helloDecodedDoneResult);
-            Add(Input(encodedString, true), helloDecodedDoneResult);
             Add(Input(encodedString, false), helloDecodedDoneResult);
             Add(Input(encodedString, true), helloDecodedDoneResult);
 
@@ -84,8 +70,6 @@
 
             Add(Input(encodedString, false), needMoreDataResult);
             Add(Input(encodedString, true), invalidDataResult);
-            Add(Input(encodedString, false), needMoreDataResult);
-            Add(Input(encodedString, true), invalidDataResult);
 
 
             //
@@ -93,26 +77,20 @@
             //
             encodedString = Z85eSampleData.GetHelloString(7);
             needMoreDataResult = new DecodeExpectedData(OperationStatus.NeedMoreData, 5, 4, Z85eSampleData.HelloWorldBytes(4));
-            invalidDataResult = new DecodeExpectedData(OperationStatus.InvalidData, 5, 4, Z85eSampleData.HelloWorldBytes(4));
             var paddedFiveByteFinalResult = new DecodeExpectedData(OperationStatus.Done, 7, 5, Z85eSampleData.HelloWorldBytes(5));
 
             Add(Input(encodedString, false), needMoreDataResult);
             Add(Input(encodedString, true), paddedFiveByteFinalResult);  // different
-            Add(Input(encodedString, false), needMoreDataResult);
-            Add(Input(encodedString, true), invalidDataResult);
 
             //
             // Decode 8 chars.
             //
             encodedString = Z85eSampleData.GetHelloString(8);
             needMoreDataResult = new DecodeExpectedData(OperationStatus.NeedMoreData, 5, 4, Z85eSampleData.HelloWorldBytes(4));
-            invalidDataResult = new DecodeExpectedData(OperationStatus.InvalidData, 5, 4, Z85eSampleData.HelloWorldBytes(4));
             var paddedSixByteFinalResult = new DecodeExpectedData(OperationStatus.Done, 8, 6, Z85eSampleData.HelloWorldBytes(6));
 
             Add(Input(encodedString, false), needMoreDataResult);
             Add(Input(encodedString, true), paddedSixByteFinalResult);  // different
-            Add(Input(encodedString, false), needMoreDataResult);
-            Add(Input(encodedString, true), invalidDataResult);
 
 
             //
@@ -120,25 +98,18 @@
             //
             encodedString = Z85eSampleData.GetHelloString(9);
             needMoreDataResult = new DecodeExpectedData(OperationStatus.NeedMoreData, 5, 4, Z85eSampleData.HelloWorldBytes(4));
-            invalidDataResult = new DecodeExpectedData(OperationStatus.InvalidData, 5, 4, Z85eSampleData.HelloWorldBytes(4));
             var paddedSevenByteFinalResult = new DecodeExpectedData(OperationStatus.Done, 9, 7, Z85eSampleData.HelloWorldBytes(7));
 
             Add(Input(encodedString, false), needMoreDataResult);
             Add(Input(encodedString, true), paddedSevenByteFinalResult);  // different
-            Add(Input(encodedString, false), needMoreDataResult);
-            Add(Input(encodedString, true), invalidDataResult);
 
 
             //
             // Decode 10 chars.
-            // This is allowed in strict Z85
-            // In all cases the result should be the same.
             //
             encodedString = Z85eSampleData.GetHelloString(10);
             var helloWorldDecodedDoneResult = new DecodeExpectedData(OperationStatus.Done, 10, 8, Z85eSampleData.HelloWorldBytes(8));
 
-            Add(Input(encodedString, false), helloWorldDecodedDoneResult);
-            Add(Input(encodedString, true), helloWorldDecodedDoneResult);
             Add(Input(encodedString, false), helloWorldDecodedDoneResult);
             Add(Input(encodedString, true), helloWorldDecodedDoneResult);
         }
